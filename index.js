4e6c5d0fdc5a9c6f5d859db6b39d4265bf194e7a
Justin Klemm <jwklemm@gmail.com>
2018-06-26 13:45:19 -0700

    Convert CoffeeScript to JS w/decaffeinate

--- index.js
+++ index.js
@@ -1,192 +1,173 @@
-var GhostInspector, fs, https;
+/*
+ * decaffeinate suggestions:
+ * DS101: Remove unnecessary use of Array.from
+ * DS102: Remove unnecessary code created because of implicit returns
+ * DS206: Consider reworking classes to avoid initClass
+ * DS207: Consider shorter variations of null checks
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+const https = require('https');
+const fs = require('fs');
 
-https = require('https');
 
-fs = require('fs');
+// Define GhostInspector class
+class GhostInspector {
+  static initClass() {
+    this.prototype.host =   'https://api.ghostinspector.com';
+    this.prototype.prefix = '/v1';
+  }
 
-GhostInspector = (function() {
-  // Define GhostInspector class
-  class GhostInspector {
-    constructor(apiKey) {
-      this.apiKey = apiKey;
-    }
+  constructor(apiKey) {
+    this.apiKey = apiKey;
+  }
 
-    buildRequestUrl(path, params = {}) {
-      var i, item, key, len, url, val;
-      // Build request URL
-      url = this.host + this.prefix + path + '?';
-      // Add auth and other params
-      params.apiKey = this.apiKey;
-      for (key in params) {
-        val = params[key];
-        // handle array params
-        if (val instanceof Array) {
-          for (i = 0, len = val.length; i < len; i++) {
-            item = val[i];
-            url += key + '[]=' + encodeURIComponent(item) + '&';
-          }
-        } else {
-          url += key + '=' + encodeURIComponent(val) + '&';
+  buildRequestUrl(path, params) {
+    // Build request URL
+    if (params == null) { params = {}; }
+    let url = this.host + this.prefix + path + '?';
+    // Add auth and other params
+    params.apiKey = this.apiKey;
+    for (let key in params) {
+      // handle array params
+      const val = params[key];
+      if (val instanceof Array) {
+        for (let item of Array.from(val)) {
+          url += key + '[]=' + encodeURIComponent(item) + '&';
         }
+      } else {
+        url += key + '=' + encodeURIComponent(val) + '&';
       }
-      return url;
     }
+    return url;
+  }
 
-    request(path, params, callback) {
-      var url;
-      // Sort out params and callback
-      if (typeof params === 'function') {
-        callback = params;
-        params = {};
-      } else if (!params || typeof params !== 'object') {
-        params = {};
-      }
-      // Send request to API
-      url = this.buildRequestUrl(path, params);
-      return https.get(url, function(res) {
-        var json;
-        json = '';
-        // Set long timeout (30 mins)
-        res.setTimeout(1800000);
-        // Get response
-        res.on('data', function(data) {
-          return json += data;
-        });
-        // Process response
-        return res.on('end', function() {
-          var err, result;
-          try {
-            result = JSON.parse(json);
-          } catch (error) {
-            err = error;
-            result = {
-              code: 'ERROR',
-              message: 'The Ghost Inspector service is not returning a valid response.'
-            };
-          }
-          if (result.code === 'ERROR') {
-            return typeof callback === "function" ? callback(result.message) : void 0;
-          }
-          return typeof callback === "function" ? callback(null, result.data) : void 0;
-        });
-      }).on('error', function(err) {
-        return typeof callback === "function" ? callback(err) : void 0;
-      });
+  request(path, params, callback) {
+    // Sort out params and callback
+    if (typeof params === 'function') {
+      callback = params;
+      params = {};
+    } else if (!params || (typeof params !== 'object')) {
+      params = {};
     }
-
-    download(path, dest, callback) {
-      var file, url;
-      file = fs.createWriteStream(dest);
-      // Send request to API
-      url = this.buildRequestUrl(path);
-      return https.get(url, function(res) {
-        // Save response into file
-        res.pipe(file);
-        return file.on('finish', function() {
-          return file.close(callback);
-        });
-      }).on('error', function(err) {
-        return typeof callback === "function" ? callback(err) : void 0;
-      });
-    }
-
-    getSuites(callback) {
-      return this.request('/suites/', callback);
-    }
-
-    getSuite(suiteId, callback) {
-      return this.request('/suites/' + suiteId + '/', callback);
-    }
-
-    getSuiteTests(suiteId, callback) {
-      return this.request('/suites/' + suiteId + '/tests/', callback);
-    }
-
-    executeSuite(suiteId, options, callback) {
-      // Sort out options and callback
-      if (typeof options === 'function') {
-        callback = options;
-        options = {};
-      }
-      // Execute API call
-      return this.request('/suites/' + suiteId + '/execute/', options, function(err, data) {
-        var i, len, passing, test;
-        if (err) {
-          return typeof callback === "function" ? callback(err) : void 0;
+    // Send request to API
+    const url = this.buildRequestUrl(path, params);
+    return https.get(url, function(res) {
+      let json = '';
+      // Set long timeout (30 mins)
+      res.setTimeout(1800000);
+      // Get response
+      res.on('data', data => json += data);
+      // Process response
+      return res.on('end', function() {
+        let result;
+        try {
+          result = JSON.parse(json);
+        } catch (err) {
+          result = {
+            code: 'ERROR',
+            message: 'The Ghost Inspector service is not returning a valid response.'
+          };
         }
-        // Check test results, determine overall pass/fail
-        if (data instanceof Array) {
-          passing = true;
-          for (i = 0, len = data.length; i < len; i++) {
-            test = data[i];
-            passing = passing && test.passing;
-          }
-        } else {
-          passing = null;
-        }
-        return typeof callback === "function" ? callback(null, data, passing) : void 0;
+        if (result.code === 'ERROR') { return (typeof callback === 'function' ? callback(result.message) : undefined); }
+        return (typeof callback === 'function' ? callback(null, result.data) : undefined);
       });
-    }
+  }).on('error', err => typeof callback === 'function' ? callback(err) : undefined);
+  }
 
-    downloadSuiteSeleniumHtml(suiteId, dest, callback) {
-      return this.download('/suites/' + suiteId + '/export/selenium-html/', dest, callback);
-    }
+  download(path, dest, callback) {
+    const file = fs.createWriteStream(dest);
+    // Send request to API
+    const url = this.buildRequestUrl(path);
+    return https.get(url, function(res) {
+      // Save response into file
+      res.pipe(file);
+      return file.on('finish', () => file.close(callback));
+  }).on('error', err => typeof callback === 'function' ? callback(err) : undefined);
+  }
 
-    getTests(callback) {
-      return this.request('/tests/', callback);
-    }
+  getSuites(callback) {
+    return this.request('/suites/', callback);
+  }
 
-    getTest(testId, callback) {
-      return this.request('/tests/' + testId + '/', callback);
-    }
+  getSuite(suiteId, callback) {
+    return this.request(`/suites/${suiteId}/`, callback);
+  }
 
-    getTestResults(testId, options, callback) {
-      // Sort out options and callback
-      if (typeof options === 'function') {
-        callback = options;
-        options = {};
-      }
-      // Execute API call
-      return this.request('/tests/' + testId + '/results/', options, callback);
-    }
+  getSuiteTests(suiteId, callback) {
+    return this.request(`/suites/${suiteId}/tests/`, callback);
+  }
 
-    executeTest(testId, options, callback) {
-      // Sort out options and callback
-      if (typeof options === 'function') {
-        callback = options;
-        options = {};
-      }
-      // Execute API call
-      return this.request('/tests/' + testId + '/execute/', options, function(err, data) {
-        var passing;
-        if (err) {
-          return typeof callback === "function" ? callback(err) : void 0;
+  executeSuite(suiteId, options, callback) {
+    // Sort out options and callback
+    if (typeof options === 'function') {
+      callback = options;
+      options = {};
+    }
+    // Execute API call
+    return this.request(`/suites/${suiteId}/execute/`, options, function(err, data) {
+      let passing;
+      if (err) { return (typeof callback === 'function' ? callback(err) : undefined); }
+      // Check test results, determine overall pass/fail
+      if (data instanceof Array) {
+        passing = true;
+        for (let test of Array.from(data)) {
+          passing = passing && test.passing;
         }
-        // Call back with extra pass/fail parameter
-        passing = data.passing === void 0 ? null : data.passing;
-        return typeof callback === "function" ? callback(null, data, passing) : void 0;
-      });
+      } else {
+        passing = null;
+      }
+      // Call back with extra pass/fail parameter
+      return (typeof callback === 'function' ? callback(null, data, passing) : undefined);
+    });
+  }
+
+  downloadSuiteSeleniumHtml(suiteId, dest, callback) {
+    return this.download(`/suites/${suiteId}/export/selenium-html/`, dest, callback);
+  }
+
+  getTests(callback) {
+    return this.request('/tests/', callback);
+  }
+
+  getTest(testId, callback) {
+    return this.request(`/tests/${testId}/`, callback);
+  }
+
+  getTestResults(testId, options, callback) {
+    // Sort out options and callback
+    if (typeof options === 'function') {
+      callback = options;
+      options = {};
     }
+    // Execute API call
+    return this.request(`/tests/${testId}/results/`, options, callback);
+  }
 
-    downloadTestSeleniumHtml(testId, dest, callback) {
-      return this.download('/tests/' + testId + '/export/selenium-html/', dest, callback);
+  executeTest(testId, options, callback) {
+    // Sort out options and callback
+    if (typeof options === 'function') {
+      callback = options;
+      options = {};
     }
+    // Execute API call
+    return this.request(`/tests/${testId}/execute/`, options, function(err, data) {
+      if (err) { return (typeof callback === 'function' ? callback(err) : undefined); }
+      // Call back with extra pass/fail parameter
+      const passing = data.passing === undefined ? null : data.passing;
+      return (typeof callback === 'function' ? callback(null, data, passing) : undefined);
+    });
+  }
 
-    getResult(resultId, callback) {
-      return this.request('/results/' + resultId + '/', callback);
-    }
+  downloadTestSeleniumHtml(testId, dest, callback) {
+    return this.download(`/tests/${testId}/export/selenium-html/`, dest, callback);
+  }
 
-  };
+  getResult(resultId, callback) {
+    return this.request(`/results/${resultId}/`, callback);
+  }
+}
+GhostInspector.initClass();
 
-  GhostInspector.prototype.host = 'https://api.ghostinspector.com';
-
-  GhostInspector.prototype.prefix = '/v1';
-
-  return GhostInspector;
-
-}).call(this);
 
 // Export new GhostInspector instance
-module.exports = function(param1, param2) {
-  return new GhostInspector(param2 ? param2 : param1);
-};
+module.exports = (param1, param2) => new GhostInspector(param2 ? param2 : param1);
--- test/test.js
+++ test/test.js
@@ -1,195 +1,187 @@
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
 // The API key and IDs used in this file belong to the official Ghost Inspector API testing account.
-var GhostInspector, fs, should;
+const fs = require('fs');
+const should = require('should');
+const GhostInspector = require('../index')('ff586dcaaa9b781163dbae48a230ea1947f894ff');
 
-fs = require('fs');
-
-should = require('should');
-
-GhostInspector = require('../index')('ff586dcaaa9b781163dbae48a230ea1947f894ff');
 
 describe('Get suites', function() {
   this.timeout(0);
-  return it('should return 1 suite', function(done) {
-    return GhostInspector.getSuites(function(err, data) {
+  return it('should return 1 suite', done =>
+    GhostInspector.getSuites(function(err, data) {
       (err === null).should.be.true;
       data.length.should.equal(1);
       return done();
-    });
-  });
+    })
+  );
 });
 
 describe('Get suite', function() {
   this.timeout(0);
-  return it('should return a suite named "Test Suite"', function(done) {
-    return GhostInspector.getSuite('53cf58c0350c6c41029a11be', function(err, data) {
+  return it('should return a suite named "Test Suite"', done =>
+    GhostInspector.getSuite('53cf58c0350c6c41029a11be', function(err, data) {
       (err === null).should.be.true;
       data.name.should.equal("Test Suite");
       return done();
-    });
-  });
+    })
+  );
 });
 
 describe('Get suite tests', function() {
   this.timeout(0);
-  return it('should return 2 tests in the suite', function(done) {
-    return GhostInspector.getSuiteTests('53cf58c0350c6c41029a11be', function(err, data) {
+  return it('should return 2 tests in the suite', done =>
+    GhostInspector.getSuiteTests('53cf58c0350c6c41029a11be', function(err, data) {
       (err === null).should.be.true;
       data.length.should.equal(2);
       return done();
-    });
-  });
+    })
+  );
 });
 
 describe('Execute suite ', function() {
   this.timeout(0);
-  return it('should return 2 results and a passing status', function(done) {
-    return GhostInspector.executeSuite('53cf58c0350c6c41029a11be', function(err, data, passing) {
+  return it('should return 2 results and a passing status', done =>
+    GhostInspector.executeSuite('53cf58c0350c6c41029a11be', function(err, data, passing) {
       (err === null).should.be.true;
       data.length.should.equal(2);
       passing.should.be.true;
       return done();
-    });
-  });
+    })
+  );
 });
 
 describe('Execute suite with immediate response ', function() {
   this.timeout(0);
-  return it('should return success with a pending suite result and null passing value', function(done) {
-    return GhostInspector.executeSuite('53cf58c0350c6c41029a11be', {
-      immediate: true
-    }, function(err, data, passing) {
+  return it('should return success with a pending suite result and null passing value', done =>
+    GhostInspector.executeSuite('53cf58c0350c6c41029a11be', { immediate: true }, function(err, data, passing) {
       (err === null).should.be.true;
       data.suite.should.equal('53cf58c0350c6c41029a11be');
       data.name.should.equal('Test Suite');
       (data.passing === null).should.be.true;
       (passing === null).should.be.true;
       return done();
-    });
-  });
+    })
+  );
 });
 
 describe('Download suite in (zipped) Selenium format', function() {
-  var dest;
   this.timeout(0);
-  dest = 'test/suite.zip';
-  return it('should return a zip file', function(done) {
-    return GhostInspector.downloadSuiteSeleniumHtml('53cf58c0350c6c41029a11be', dest, function(err) {
+  const dest = 'test/suite.zip';
+  return it('should return a zip file', done =>
+    GhostInspector.downloadSuiteSeleniumHtml('53cf58c0350c6c41029a11be', dest, function(err) {
       (err === null).should.be.true;
       fs.existsSync(dest).should.be.true;
       fs.unlinkSync(dest);
       return done();
-    });
-  });
+    })
+  );
 });
 
 describe('Get tests', function() {
   this.timeout(0);
-  return it('should return 2 tests', function(done) {
-    return GhostInspector.getTests(function(err, data) {
+  return it('should return 2 tests', done =>
+    GhostInspector.getTests(function(err, data) {
       (err === null).should.be.true;
       data.length.should.equal(2);
       return done();
-    });
-  });
+    })
+  );
 });
 
 describe('Get test', function() {
   this.timeout(0);
-  return it('should return a test named "Google"', function(done) {
-    return GhostInspector.getTest('53cf58fc350c6c41029a11bf', function(err, data) {
+  return it('should return a test named "Google"', done =>
+    GhostInspector.getTest('53cf58fc350c6c41029a11bf', function(err, data) {
       (err === null).should.be.true;
       data.name.should.equal("Google");
       return done();
-    });
-  });
+    })
+  );
 });
 
 describe('Get test results', function() {
   this.timeout(0);
-  return it('should return at least 1 result with a test name of "Google"', function(done) {
-    return GhostInspector.getTestResults('53cf58fc350c6c41029a11bf', function(err, data) {
+  return it('should return at least 1 result with a test name of "Google"', done =>
+    GhostInspector.getTestResults('53cf58fc350c6c41029a11bf', function(err, data) {
       (err === null).should.be.true;
       data[0].test.name.should.equal("Google");
       return done();
-    });
-  });
+    })
+  );
 });
 
 describe('Get test results with options', function() {
   this.timeout(0);
-  return it('should return 1 result', function(done) {
-    return GhostInspector.getTestResults('53cf58fc350c6c41029a11bf', {
-      'count': 1
-    }, function(err, data) {
+  return it('should return 1 result', done =>
+    GhostInspector.getTestResults('53cf58fc350c6c41029a11bf', { 'count': 1 }, function(err, data) {
       (err === null).should.be.true;
       data.length.should.equal(1);
       return done();
-    });
-  });
+    })
+  );
 });
 
 describe('Execute test ', function() {
   this.timeout(0);
-  return it('should return a test name of "Google" and a passing status', function(done) {
-    return GhostInspector.executeTest('53cf58fc350c6c41029a11bf', function(err, data, passing) {
+  return it('should return a test name of "Google" and a passing status', done =>
+    GhostInspector.executeTest('53cf58fc350c6c41029a11bf', function(err, data, passing) {
       (err === null).should.be.true;
       data.test.name.should.equal("Google");
       passing.should.be.true;
       return done();
-    });
-  });
+    })
+  );
 });
 
 describe('Execute test overriding start URL ', function() {
   this.timeout(0);
-  return it('should return a start URL of "https://www.google.com.br"', function(done) {
-    return GhostInspector.executeTest('53cf58fc350c6c41029a11bf', {
-      startUrl: 'https://www.google.com.br'
-    }, function(err, data, passing) {
+  return it('should return a start URL of "https://www.google.com.br"', done =>
+    GhostInspector.executeTest('53cf58fc350c6c41029a11bf', { startUrl: 'https://www.google.com.br' }, function(err, data, passing) {
       (err === null).should.be.true;
       data.startUrl.should.equal("https://www.google.com.br");
       passing.should.be.true;
       return done();
-    });
-  });
+    })
+  );
 });
 
 describe('Execute test with immediate response ', function() {
   this.timeout(0);
-  return it('should return success with a pending result and null passing value', function(done) {
-    return GhostInspector.executeTest('53cf58fc350c6c41029a11bf', {
-      immediate: true
-    }, function(err, data, passing) {
+  return it('should return success with a pending result and null passing value', done =>
+    GhostInspector.executeTest('53cf58fc350c6c41029a11bf', { immediate: true }, function(err, data, passing) {
       (err === null).should.be.true;
       data.test.should.equal('53cf58fc350c6c41029a11bf');
       data.name.should.equal('Google');
       (data.passing === null).should.be.true;
       (passing === null).should.be.true;
       return done();
-    });
-  });
+    })
+  );
 });
 
 describe('Download test in Selenium format', function() {
-  var dest;
   this.timeout(0);
-  dest = 'test/test.html';
-  return it('should return an HTML document', function(done) {
-    return GhostInspector.downloadTestSeleniumHtml('53cf58fc350c6c41029a11bf', dest, function(err) {
+  const dest = 'test/test.html';
+  return it('should return an HTML document', done =>
+    GhostInspector.downloadTestSeleniumHtml('53cf58fc350c6c41029a11bf', dest, function(err) {
       (err === null).should.be.true;
       fs.existsSync(dest).should.be.true;
       fs.unlinkSync(dest);
       return done();
-    });
-  });
+    })
+  );
 });
 
 describe('Get result ', function() {
   this.timeout(0);
-  return it('should return an error that the result does not exist', function(done) {
-    return GhostInspector.getResult('53cf58fe8e871daa3d95c6c5', function(err, data) {
+  return it('should return an error that the result does not exist', done =>
+    GhostInspector.getResult('53cf58fe8e871daa3d95c6c5', function(err, data) {
       err.should.equal("Result not found");
       return done();
-    });
-  });
+    })
+  );
 });
