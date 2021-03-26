9635436948ca0da0522de139baf3c1de88724a18
Aaron Fay <aaron.j.fay@gmail.com>
2021-03-26 13:04:48 -0700

    add CLI method for getAllRunningTests()

--- bin/commands/organization/get-running.js
+++ bin/commands/organization/get-running.js
@@ -1,13 +1,19 @@
 
 const helpers = require('../../helpers')
 
-// TODO
 module.exports = {
   command: 'get-running <organization-id>',
-  desc: 'Get all running results within your organization.',
+  desc: 'Fetch a list of the currently-executing results for the entire organization and return the results.',
   builder: {},
   handler: async function (argv) {
-    console.log('ERROR: getRunning() not yet implemented in node-ghost-inspector')
-    process.exit(1)
+    try {
+      const client = require('../../../index')(argv.apiKey)
+      const result = await client.getAllRunningTests(argv.organizationId)
+      helpers.print(result)
+    } catch (error) {
+      throw new Error(error.message)
+    }
+
+    process.exit(0)
   }
 }
