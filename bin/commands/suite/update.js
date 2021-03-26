a6f317d09780d1f89cb47981ebaeb4002a3b2996
Aaron Fay <aaron.j.fay@gmail.com>
2021-03-26 13:13:45 -0700

    add CLI method for updateSuite()

--- bin/commands/suite/update.js
+++ bin/commands/suite/update.js
@@ -1,13 +1,18 @@
 
 const helpers = require('../../helpers')
 
-// TODO
 module.exports = {
-  command: 'update <suite-id>',
-  desc: 'Update a suite.',
+  command: 'update <suite-id> <suite-name>',
+  desc: 'Update a suite with new <suite-name>.',
   builder: {},
   handler: async function (argv) {
-    console.log('ERROR: updateSuite() not yet implemented in node-ghost-inspector')
-    process.exit(1)
+    try {
+      const client = require('../../../index')(argv.apiKey)
+      const result = await client.updateSuite(argv.suiteId, argv.suiteName)
+      helpers.print(result)
+    } catch (error) {
+      throw new Error(error.message)
+    }
+    process.exit(0)
   }
 }
