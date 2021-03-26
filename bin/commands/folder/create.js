f7df7ad6c393875dc63b5e0d6bc65170872a0542
Aaron Fay <aaron.j.fay@gmail.com>
2021-03-26 12:49:11 -0700

    adds cli method for create folder

--- bin/commands/folder/create.js
+++ bin/commands/folder/create.js
@@ -1,13 +1,18 @@
 
 const helpers = require('../../helpers')
 
-// TODO
 module.exports = {
-  command: 'create <folder-id>',
+  command: 'create <organization-id> <folder-name>',
   desc: 'Create a folder.',
   builder: {},
   handler: async function (argv) {
-    console.log('ERROR: createFolder() not yet implemented in node-ghost-inspector')
-    process.exit(1)
+    try {
+      const client = require('../../../index')(argv.apiKey)
+      const result = await client.createFolder(argv.organizationId, argv.folderName)
+      helpers.print(result)
+    } catch (error) {
+      throw new Error(error.message)
+    }
+    process.exit(0)
   }
 }
